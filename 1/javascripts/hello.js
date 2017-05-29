ccf51322a57cbd5d4c736a04f6a87e25a8b779a4
IchikiRyo <tohodaiichiki@gmail.com>
2017-05-29 20:13:30 +0900

    Add basement

---
+++ 1/javascripts/hello.js
@@ -0,0 +1,9 @@
+$(function() {
+  var helloWorldController = {
+    __name: 'HelloWorldController', 
+    '#button click': function() {
+      alert("こんにちは世界！");
+    }
+  };
+  h5.core.controller('body', helloWorldController);
+});
