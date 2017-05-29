40811fa84a76c96b93990d65141e2a229769e057
IchikiRyo <tohodaiichiki@gmail.com>
2017-05-29 20:15:18 +0900

    Add basement

--- 1/javascripts/hello.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  var helloWorldController = {
-    __name: 'HelloWorldController', 
-    '#button click': function() {
-      alert("こんにちは世界！");
-    }
-  };
-  h5.core.controller('body', helloWorldController);
-});
